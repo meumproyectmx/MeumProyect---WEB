1ff65321589fa769f5154edf2001b313d7574e63
Ing.Germán Ruz <meumproyect@gmail.com>
2026-03-29 01:45:41 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/* 1-BANNER (Lógica de Fecha) */
+function actualizarFecha() {
+    const contenedor = document.getElementById('fecha-banner');
+    if (!contenedor) return;
+
+    const fecha = new Date();
+    const opciones = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let texto = fecha.toLocaleDateString('es-ES', opciones);
+    
+    contenedor.innerHTML = `Actualizado el: ${texto.charAt(0).toUpperCase() + texto.slice(1)}`;
+}
+
+document.addEventListener('DOMContentLoaded', actualizarFecha);
